90646a22670e7a81fab182ff9a0aeb91e6ff9abe
Alex Monteiro <alexjmcm@gmail.com>
2021-10-18 21:49:02 -0400

    Created PygameRPG

---
+++ config.py
@@ -0,0 +1,35 @@
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+TILESIZE = 32
+FPS = 60
+
+BLOCK_LAYER = 2
+GROUND_LAYER = 1
+ENEMY_LAYER = 3
+PLAYER_LAYER = 4
+
+PLAYER_SPEED = 3
+ENEMY_SPEED = 2
+
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B..E...............B',
+    'B................B.B',
+    'B....B...........B.B',
+    'B....B...........B.B',
+    'B....B...BBBBBBB.B.B',
+    'B................B.B',
+    'B.........P......B.B',
+    'B...BBBBBB.......B.B',
+    'B.............E..B.B',    
+    'B................B.B',
+    'B......BBBBBBBBB.B.B',
+    'B....E...........B.B',   
+    'B..................B',   
+    'BBBBBBBBBBBBBBBBBBBB',
+]
